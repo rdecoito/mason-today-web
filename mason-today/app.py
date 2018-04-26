b40071ef345e6d76243a5bc16f5a2bad966cea34
Landon DeCoito <rdecoito@gmu.edu>
2018-04-25 20:49:39 -0400

    made a default welcome page

--- mason-today/app.py
+++ mason-today/app.py
@@ -6,11 +6,18 @@ import json
 
 app = Flask(__name__)
 
+@app.route("/")
+def display_default():
+	resp = Response(("Welcome to the masontoday API! Go to https://git.gmu.edu/srct/mason-today-web <br/><br/>"
+					+ "Feel free to go to /api/25live/ or /api/getconnected/ to find our api!").encode('utf-8'))
+	return resp
+
 @app.route("/api/25live")
 def display_data():
     resp = Response(json.dumps(load_data(), ensure_ascii=False).encode('utf-8'))
     resp.headers['Content-Type'] = 'application/json; charset=utf-8'
     return resp
+
 
 @app.route("/api/getconnected")
 def display_GC_data():
