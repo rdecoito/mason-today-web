626a818c1e3974e53bfa279d0f04f3084201c828
Landon DeCoito <rdecoito@gmu.edu>
2018-04-26 15:21:22 -0400

    moved some stuff around i think

--- mason-today/app.py
+++ mason-today/app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def display_default():
-	resp = Response(("Welcome to the masontoday API! Go to https://git.gmu.edu/srct/mason-today-web <br/><br/>"
-					+ "Feel free to go to /api/25live/ or /api/getconnected/ to find our api!").encode('utf-8'))
-	return resp
+    resp = Response(("Welcome to the masontoday API! Go to https://git.gmu.edu/srct/mason-today-web <br/><br/>"
+                    + "Feel free to go to /api/25live/ or /api/getconnected/ to find our api!").encode('utf-8'))
+    return resp
 
 @app.route("/api/25live")
 def display_data():
@@ -21,6 +21,6 @@ def display_data():
 
 @app.route("/api/getconnected")
 def display_GC_data():
-	resp = Response(json.dumps(load_getconn_data(), ensure_ascii=False).encode('utf-8'))
-	resp.headers['Content-Type'] = 'application/json; charset=utf-8'
-	return resp
+    resp = Response(json.dumps(load_getconn_data(), ensure_ascii=False).encode('utf-8'))
+    resp.headers['Content-Type'] = 'application/json; charset=utf-8'
+    return resp
