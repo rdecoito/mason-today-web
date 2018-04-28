0c0bc39c3b6e759a1ba347d3dcafdd0205508f31
Landon DeCoito <rdecoito@gmu.edu>
2018-04-27 20:54:59 -0400

    renamed app.py to __init__.py and changed start.sh to comply

--- mason-today/__init__.py
+++ mason-today/__init__.py
@@ -1,26 +1,35 @@
+# flask imports
 from flask import Flask
 from flask import Response
+from flask import render_template
+
+# app imports
 from parscript import load_data
 from getconnectedscript import load_getconn_data
+
+# python imports
 import json
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def display_default():
-    resp = Response(("Welcome to the masontoday API! Go to https://git.gmu.edu/srct/mason-today-web <br/><br/>"
-                    + "Feel free to go to /api/25live/ or /api/getconnected/ to find our api!").encode('utf-8'))
+    resp = render_template('welcomepage.html')
     return resp
+
 
 @app.route("/api/25live")
 def display_data():
-    resp = Response(json.dumps(load_data(), ensure_ascii=False).encode('utf-8'))
+    resp = Response(json.dumps(load_data(), ensure_ascii=False)
+                    .encode('utf-8'))
     resp.headers['Content-Type'] = 'application/json; charset=utf-8'
     return resp
 
 
 @app.route("/api/getconnected")
 def display_GC_data():
-    resp = Response(json.dumps(load_getconn_data(), ensure_ascii=False).encode('utf-8'))
+    resp = Response(json.dumps(load_getconn_data(), ensure_ascii=False)
+                    .encode('utf-8'))
     resp.headers['Content-Type'] = 'application/json; charset=utf-8'
     return resp
