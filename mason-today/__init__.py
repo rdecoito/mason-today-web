f5408b5ba9e67a01d329aaef3202f3fec8ecd65c
Landon DeCoito <rdecoito@gmu.edu>
2018-07-12 22:06:13 -0400

    added scheduling to the __init__

--- mason-today/__init__.py
+++ mason-today/__init__.py
@@ -6,19 +6,27 @@ from flask import render_template
 # app imports
 from parscript import load_data
 from getconnectedscript import load_getconn_data
+from redisactions import *
+from appmethods import *
 
 # python imports
 import json
 
 # other imports
 import redis
-from redisactions import *
+import schedule
+import time
 
 # setting up flask instance
 app = Flask(__name__)
 
 # setting up redis database
 redisdb = redis.from_url("redis://localhost:6379/0", db=0)
+
+# setting up cacheing
+schedule.every().day.at("02:00").do(updatebothdbs)
+# schedule.every(5).seconds.do(updatebothdbs)
+
 
 @app.route("/")
 def display_default():
@@ -28,15 +36,19 @@ def display_default():
 
 @app.route("/api/25live")
 def display_data():
-    livedbfill(json.dumps(load_data(), ensure_ascii=False))
-    resp = Response(redisdb.get("livedict")) # .encode('utf-8'))
+    resp = Response(redisdb.get("livedict"))  # .encode('utf-8'))
     resp.headers['Content-Type'] = 'application/json; charset=utf-8'
     return resp
 
 
 @app.route("/api/getconnected")
 def display_GC_data():
-    gcdbfill(json.dumps(load_getconn_data(), ensure_ascii=False))
-    resp = Response(redisdb.get("gcdict")) # .encode('utf-8'))
+    resp = Response(redisdb.get("gcdict"))  # .encode('utf-8'))
     resp.headers['Content-Type'] = 'application/json; charset=utf-8'
     return resp
+
+# this needs to be uncommented in order for the scheduler to work
+# but it's being weird cause it's hogging the thread
+# while True:
+    # schedule.run_pending()
+    # time.sleep(5)
